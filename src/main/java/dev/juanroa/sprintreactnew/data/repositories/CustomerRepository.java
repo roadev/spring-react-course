29b408a6ae51e09856fa1c2b114b7f3beb707c49
Juan Roa <hello@juanroa.dev>
2024-06-02 17:03:17 -0500

    working on customers entity and schema

---
+++ src/main/java/dev/juanroa/sprintreactnew/data/repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+package dev.juanroa.sprintreactnew.data.repositories;
+
+import dev.juanroa.sprintreactnew.data.entities.CustomerEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
+
+    CustomerEntity findByEmail(String email);
+
+}
+
