f789477274d592851021c07563e0820fd658195c
Juan Roa <hello@juanroa.dev>
2024-06-02 16:21:49 -0500

    initial commit, adding react to maven

---
+++ src/main/java/dev/juanroa/sprintreactnew/SprintReactNewApplication.java
@@ -0,0 +1,13 @@
+package dev.juanroa.sprintreactnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintReactNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintReactNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/juanroa/sprintreactnew/SprintReactNewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juanroa.sprintreactnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintReactNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
